7ce9cdbc49d017147367d0832afd72ff93924965
24hoursmedia <info@24hoursmedia.com>
2016-08-08 13:54:53 +0200

    version in build commands

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # build file
-docker build .  -t 24hoursmedia/dxstack1-base:development
+docker build .  -t 24hoursmedia/dxstack1-base:v1.0-dev
--- buildandtest.sh
+++ buildandtest.sh
@@ -3,4 +3,4 @@
 # builds the image, and runs a test container named dxstack1_base_test (removes it first if it exists)
 
 docker rm dxstack1_base_dev || echo ''
-docker build .  -t 24hoursmedia/dxstack1-base:development && docker run --name=dxstack1_base_dev -ti 24hoursmedia/dxstack1-base:development
+docker build .  -t 24hoursmedia/dxstack1-base:v1.0-dev && docker run --name=dxstack1_base_dev -ti 24hoursmedia/dxstack1-base:v1.0-dev
